9d1cea2bc64650bfeee5a717e9d1881b4227b864
czarryy2010 <czarryy2009@yahoo.com>
2011-04-24 16:59:43 -0700

    Edited config/environment.rb via GitHub

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -13,10 +13,10 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
 
   #SITE_URL = 'http://localhost:3000/'
-  #FACEBOOK_URL = "http://apps.facebook.com/testlittlesurpize/" 
+  #FACEBOOK_URL = "http://apps.facebook.comheartedgiftapp/" 
   
   SITE_URL = 'http://69.164.222.92:3000/'
-  FACEBOOK_URL = "http://apps.facebook.com/littlesurprizes/"
+  FACEBOOK_URL = "http://apps.facebook.com/heartedgiftapp/"
 
   config.gem "authlogic"
 
